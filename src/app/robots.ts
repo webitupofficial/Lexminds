526aac905537ed693356ba8398af731e15cca563
Lexminds <contact@lexminds.in>
2026-09-08 18:18:33 +0530

    fix(seo): remove /_next/ disallow from robots.txt to allow Googlebot to render page assets

---
+++ src/app/robots.ts
@@ -0,0 +1,35 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://lexminds.in';
+
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/checkout', '/payment', '/admin', '/api/'],
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: ['/', '/llms.txt'],
+        disallow: ['/checkout', '/payment', '/api/'],
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: ['/', '/llms.txt'],
+        disallow: ['/checkout', '/payment', '/api/'],
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: ['/', '/llms.txt'],
+        disallow: ['/checkout', '/payment', '/api/'],
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: '/',
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
